ada0d538cbc3c18c4c6dbcdb00842f489208fa0c
gresm <78505251+gresm@users.noreply.github.com>
2022-04-14 20:42:26 +0200

    Small update

--- git_updater/__init__.py
+++ git_updater/__init__.py
@@ -14,9 +14,9 @@ def is_valid_signature(x_hub_signature, data, private_key):
     x_hub_signature and data are from the webhook payload
     private key is your webhook secret
     """
-    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    hash_algorithm, github_signature = x_hub_signature.split("=", 1)
     algorithm = hashlib.__dict__.get(hash_algorithm)
-    encoded_key = bytes(private_key, 'latin-1')
+    encoded_key = bytes(private_key, "latin-1")
     mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
     return hmac.compare_digest(mac.hexdigest(), github_signature)
 
@@ -30,12 +30,11 @@ def main():
     return "hello from sub app: git-updater"
 
 
-@app.route('/update', methods=['POST'])
+@app.route("/update", methods=["POST"])
 def webhook():
-    """
-    True route to request.
-    """
-    if request.method == 'POST':
+    """True route to request"""
+
+    if request.method == "POST":
         x_hub_signature = request.headers.get("X-Hub-Signature")
 
         if not is_valid_signature(x_hub_signature, request.data, w_secret):
@@ -44,6 +43,5 @@ def webhook():
         repo = git.Repo(repo_location)
         origin = repo.remotes.origin
         origin.pull(origin.refs[0].remote_head)
-        return 'Updated PythonAnywhere successfully', 200
-    else:
-        return 'Wrong event type', 400
+        return "Updated PythonAnywhere successfully", 200
+    return "Wrong event type", 400
