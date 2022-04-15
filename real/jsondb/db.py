6ba70407bada378d7ab22e1c3c31c0b328a02808
gresm <78505251+gresm@users.noreply.github.com>
2022-04-15 12:03:26 +0200

    Check if exists

--- real/jsondb/db.py
+++ real/jsondb/db.py
@@ -6,11 +6,19 @@ from pathlib import Path
 
 usersdb_path = Path("users.json")
 
+if not usersdb_path.exists():
+    with usersdb_path.open("w", encoding="UTF-8") as _f:
+        _f.write("{}")
+
 with usersdb_path.open("r", encoding="UTF-8") as _f:
     raw_usersdb = json.load(_f)
 
 
 challengesdb_path = Path("challenges.json")
+
+if not challengesdb_path.exists():
+    with challengesdb_path.open("w", encoding="UTF-8") as _f:
+        _f.write("{}")
 
 with challengesdb_path.open("r", encoding="UTF-8") as _f:
     raw_challengesdb = json.load(_f)
