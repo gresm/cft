75e0aff09671e03ba45eb2a9e85ed461f781295c
gresm <78505251+gresm@users.noreply.github.com>
2022-04-15 07:14:22 +0200

    Fix run.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from real import app
 from git_updater import app as app2
 
-dispatch_app = DispatcherMiddleware(app, {
+app = DispatcherMiddleware(app, {
     '/git-updater': app2
 })
-
-app.wsgi_app = dispatch_app
--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-from main import app
+"""Run project"""
+from flask import Flask
+from main import app as dispatch_app
 
 
+app = Flask(__name__)
+app.wsgi_app = dispatch_app
 app.run()
