068854eec7f562dce283c7d4a4e5ef3f2e435d52
gresm <78505251+gresm@users.noreply.github.com>
2022-04-17 09:08:29 +0200

    "Small" update.

--- real/__init__.py
+++ real/__init__.py
@@ -1,23 +1,20 @@
 """Main Website"""
-from flask import Flask, render_template, request
-from .jsondb import usersdb
+from flask import Flask, render_template
+from .validate import validate
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
+@validate(missing_template="index-missing.html")
 def main():
     """Main page"""
-    if "key" in request.args:
-        if request.args["key"] in usersdb.users:
-            pass
-        else:
-            return render_template("index-invalid-user.html"), 404
     return render_template("index.html")
 
 
 @app.route("/", subdomain="<category>")
+@validate
 def main_category(category: str):
     """Main page for categories"""
     return category
---
+++ real/validate.py
@@ -0,0 +1,53 @@
+"""Utility functions for validating requests"""
+from __future__ import annotations
+from typing import Callable
+from flask import request, render_template
+
+try:
+    from .jsondb import usersdb
+except ImportError:
+    from jsondb import usersdb
+
+
+INVALID_TOKEN = "invalid-token.html"
+MISSING_TOKEN = "missing-token.html"
+
+
+def validate(
+    func: Callable | None = None,
+    /,
+    *,
+    invalid_template: str | None = None,
+    missing_template: str | None = None,
+):
+    """
+    Decorator for functions that require special verification to get access.
+    """
+
+    def internal_def(*args, **kwargs):
+        """Internal function"""
+        if "key" in request.args:
+            if request.args["key"] in usersdb.users:
+                return func(*args, **kwargs)
+            return render_template(INVALID_TOKEN), 404
+        return render_template(MISSING_TOKEN), 403
+
+    def internal_set(func):
+        """Internal function"""
+
+        invalid = invalid_template if invalid_template else INVALID_TOKEN
+        missing = missing_template if missing_template else MISSING_TOKEN
+
+        def nested(*args, **kwargs):
+            """Nested internal function"""
+            if "key" in request.args:
+                if request.args["key"] in usersdb.users:
+                    return func(*args, **kwargs)
+                return render_template(invalid), 404
+            return render_template(missing), 403
+
+        return nested
+
+    if func:
+        return internal_def
+    return internal_set
