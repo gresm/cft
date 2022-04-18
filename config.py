c0e47514d66290e61518010c192ed3acc8e11d67
gresm <78505251+gresm@users.noreply.github.com>
2022-04-18 10:59:42 +0200

    Database optional git protection.

--- config.py
+++ config.py
@@ -11,6 +11,12 @@ with convig_path.open(encoding="UTF-8") as __file:
 if "CFT_PRODUCTION" in os.environ:
     config["debug"] = False
 
+if "CFT_CUSTOM_DATABASE_USERS" in os.environ:
+    config["usersdb"] = os.environ["CFT_CUSTOM_DATABASE_USERS"]
+
+if "CFT_CUSTOM_DATABASE_CHALLENGES" in os.environ:
+    config["usersdb"] = os.environ["CFT_CUSTOM_DATABASE_CHALLENGES"]
+
 for __key in config:
     locals()[__key] = config[__key]
 
